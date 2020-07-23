d198bb221321ab66ed060106fc4bdd51d288b438
tejas002 <33129656+tejas002@users.noreply.github.com>
2020-07-23 18:35:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
 
-
 var server = app.listen(port, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
