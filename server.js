b93d8fdb1286374a1c9362d05e74ec4eed129f87
tejas <tejas.nituk@gmail.com>
2020-07-23 13:58:33 +0530

    updating the application port from env

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var app = express();
 app.use(morgan('combined'));
 app.use(express.static('public'));
 
+//setting the port
+const port = process.env.PORT || 3001;
+
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-var server = app.listen(9005, function(){
+
+
+var server = app.listen(port, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
